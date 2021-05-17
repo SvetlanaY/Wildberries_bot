903d3b709b6fc0e4dc42b56164c5e88235097198
SvetlanaIa <svetlana1905.iakovleva@yandex.ru>
2021-05-17 17:29:13 +0300

    parser.py

--- parser.py
+++ parser.py
@@ -6,15 +6,13 @@ from wildberries.wildberries.spiders.wildberries import WildberriesSpider
 
 
 def parser(final_link, file_name):
-    #file_name = f'{final_link[0][35:-7]}.jl'
-
     def crawl():
         crawler = CrawlerProcess(settings={
-                  "FEEDS": {file_name: {"format": "jl","encoding":"utf-8"},
-    },
-    },
-    )
-        crawler.crawl(WildberriesSpider,start_urls = [final_link])
+            "FEEDS": {file_name: {"format": "jl", "encoding": "utf-8"},
+                      },
+        },
+        )
+        crawler.crawl(WildberriesSpider, start_urls=[final_link])
         crawler.start()
 
     process = Process(target=crawl)
