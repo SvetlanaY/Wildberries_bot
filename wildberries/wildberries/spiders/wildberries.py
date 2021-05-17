e82f985d599c3dc2cd9edf0f7b8067d07b241994
SvetlanaIa <svetlana1905.iakovleva@yandex.ru>
2021-05-17 17:32:18 +0300

    wildberries

--- wildberries/wildberries/spiders/wildberries.py
+++ wildberries/wildberries/spiders/wildberries.py
@@ -1,4 +1,5 @@
 import json
+
 import scrapy
 from scrapy.http import JsonRequest
 
@@ -13,29 +14,37 @@ class WildberriesSpider(scrapy.Spider):
 
     def parse(self, response):
         id_api = int(response.css('div::attr(data-good-link)').get())
-        data = {"imtId":id_api,"take":self.count_comment,"order":"dateDesc"}
-        yield JsonRequest(url='https://public-feedbacks.wildberries.ru/api/v1/feedbacks/site', data=data,callback=self.parse_my_url)
-
+        data = {
+            "imtId": id_api,
+            "take": self.count_comment,
+            "order": "dateDesc"}
+        yield JsonRequest(url='https://public-feedbacks.wildberries.ru/api/v1/feedbacks/site',
+                          data=data, callback=self.parse_my_url)
 
     def parse_my_url(self, response):
         # Если сайт отдает ответ в виде json:
         data_from_json = json.loads(response.body)
-        result={}
+        result = {}
 
         for i in range(self.count_comment):
-            try :
-                res=[]
+            try:
+                res = []
                 res.append(data_from_json['feedbacks'][i].get('text'))
                 res.append(data_from_json['feedbacks'][i].get('createdDate'))
                 res.append(data_from_json['feedbacks'][i].get('color'))
                 res.append(data_from_json['feedbacks'][i].get('size'))
-                res.append(data_from_json['feedbacks'][i].get('votes').get('pluses'))
-                res.append(data_from_json['feedbacks'][i].get('votes').get('minuses'))
-                res.append(data_from_json['feedbacks'][i].get('productValuation'))
-                res.append(data_from_json['feedbacks'][i].get('productDetails').get('productName'))
-                res.append(data_from_json['feedbacks'][i].get('productDetails').get('brandName'))
-                result[i]=res
-            except:
+                res.append(data_from_json['feedbacks']
+                           [i].get('votes').get('pluses'))
+                res.append(data_from_json['feedbacks']
+                           [i].get('votes').get('minuses'))
+                res.append(data_from_json['feedbacks']
+                           [i].get('productValuation'))
+                res.append(data_from_json['feedbacks'][i].get(
+                    'productDetails').get('productName'))
+                res.append(data_from_json['feedbacks'][i].get(
+                    'productDetails').get('brandName'))
+                result[i] = res
+            except BaseException:
                 pass
 
         yield result
